7a8251d9b3899ac95a66bbdff23e24a06432b183
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-23 13:37:30 +0200

    - Rename contentView to LocationFinderView

--- LocationFinder/LocationFinderView.swift
+++ LocationFinder/LocationFinderView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LocationFinderView.swift
 //  LocationFinder
 //
 //  Created by Eric on 23/05/2023.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LocationFinderView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,8 +19,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LocationFinderView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LocationFinderView()
     }
 }
