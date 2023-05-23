b73c31078592836bcd76fb126b4b1276c65d3913
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-23 13:34:20 +0200

    Initial Commit

---
+++ LocationFinder/LocationFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationFinderApp.swift
+//  LocationFinder
+//
+//  Created by Eric on 23/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct LocationFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
